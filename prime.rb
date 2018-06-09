3bc3acb2c68e6c20769719577d83c5b9777b87f3
Shumei Huang <rose1264@gmail.com>
2018-06-09 11:37:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,9 @@
-# Add  code here!
 def prime?(number)
   # false if number <= 0
   for num in 2...number do
-    puts num
-    # if number % num == 0
-    #   false
-    # else
-    #   true
-    # end
+    if number % num == 0
+      false
+    end
   end
 end
 
